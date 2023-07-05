22f0a5e592ccbf518343a933c71d3d279847d752
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-07-05 21:28:59 +0530

    Add example.md

---
+++ example/example.md
@@ -0,0 +1,5 @@
+Refer [Usage][] section of [../README.md][]
+
+
+[Usage]: ../README.md#Usage
+[../README.md]: ../README.md
--- tool/ci.sh
+++ tool/ci.sh
@@ -6,7 +6,12 @@ dart pub get
 
 dart format --output none --set-exit-if-changed .
 
+dart analyze lib --fatal-infos
+
+pushd analysis_test &> /dev/null
+dart pub get
 dart analyze --fatal-infos
+popd &> /dev/null
 
 dart pub global activate pana
 dart pub global run pana --no-warning
